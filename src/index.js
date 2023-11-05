cc764a45fe7a6057bab8890466b651537cd7675f
Meagan <MeaganTruong1@yahoo.com>
2023-11-05 15:37:34 -0600

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,85 @@
 const init = () => {
+    const inputForm = document.querySelector("form");
   
-}
+    inputForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const input = document.querySelector("input#searchByID");
+  
+      fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const title = document.querySelector("section#movieDetails h4");
+          const summary = document.querySelector("section#movieDetails p");
+  
+          title.innerText = data.title;
+          summary.innerText = data.summary;
+        });
+    });
+  };
+  
+  document.addEventListener("DOMContentLoaded", init);
 
-document.addEventListener('DOMContentLoaded', init);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const init = () => {
+//     const inputForm = document.querySelector("form");
+//     // add event listener to capture form data / override form's default behavior
+//     inputForm.addEventListener("submit", (event) => {
+//         event.preventDefault();
+//         const input = document.querySelector("input#searchByID");
+// // fetch data based on what the user types into that form
+//         fetch(`http://localhost:3000/movies/${input.value}`)
+//         .then ((response) => response.json())
+//         .then ((data) => {
+//             const title = document.querySelector("section#movieDetails h4");
+//             const summary = document.querySelector("section#movieDetails p");
+
+//             title.innerText = data.title;
+//             summary.innerText = data.summary;
+//         });
+//     });
+// };
+// // displays the data on the page
+// document.addEventListener('DOMContentLoaded', init);
